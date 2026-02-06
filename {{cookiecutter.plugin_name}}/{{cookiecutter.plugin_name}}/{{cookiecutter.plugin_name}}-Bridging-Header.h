bdf1e400f2e31efa618bcd1a1bf86669baa4e9b7
Nathan Henrie <nate@n8henrie.com>
2026-02-06 08:37:39 -0700

    Suggest using `QS` prefix, keep this naming consistent throughout
    Previously the QS prefix was automatically inserted some places but not
    others. This way it is user-controlled by consistent.

--- {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}-Bridging-Header.h
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "QS{{cookiecutter.short_name}}.h"
+#import "{{cookiecutter.plugin_name}}.h"
 #import "Quicksilver.pch"
---
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}Source.h
@@ -0,0 +1,11 @@
+//
+//  {{cookiecutter.plugin_name}}Source.h
+//  {{cookiecutter.plugin_name}}
+//
+//  Created by {{cookiecutter.author}}
+//
+
+@interface {{cookiecutter.plugin_name}}Source : QSObjectSource
+{
+}
+@end
